cd1663ba1c28854cfed4daa834ad988821a63eb8
Basel <baselalnassr8@gmail.com>
2024-05-18 17:18:24 +0300

    working docker

--- projects/index.js
+++ projects/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser')
-const { Project } = require('schemas.js');
+const { Project } = require('./schemas.js');
 
 const app = express()
 const port = 3000
